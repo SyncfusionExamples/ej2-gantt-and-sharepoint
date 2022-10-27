fb8b4f0606c780b1a1a93cc377e1bd3d21ed2179
Alan Sangeeth S <alans@syncfusion.com>
2022-10-27 14:24:38 +0530

    added sharepoint sample with gantt in webpart

---
+++ src/webparts/ganttChart/GanttChartWebPart.ts
@@ -0,0 +1,150 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneCheckbox,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { IReadonlyTheme } from '@microsoft/sp-component-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+// import Essential JS 2 Grid
+import { Gantt, Sort } from '@syncfusion/ej2-gantt';
+
+// add Syncfusion Essential JS 2 style reference from node_modules
+require('../../../node_modules/@syncfusion/ej2/fluent.css');
+
+import styles from './GanttChartWebPart.module.scss';
+import * as strings from 'GanttChartWebPartStrings';
+
+export interface IGanttChartWebPartProps {
+  description: string;
+  allowSorting: boolean;
+}
+
+export default class GanttChartWebPart extends BaseClientSideWebPart<IGanttChartWebPartProps> {
+
+  private _isDarkTheme: boolean = false;
+  private _environmentMessage: string = '';
+
+  public render(): void {
+    this.domElement.innerHTML = `
+    <section class="${styles.ganttChart} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
+      <div class="${styles.welcome}">
+        <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
+        <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
+        <div>${this._environmentMessage}</div>
+        <div>Web part property value: <strong>${escape(this.properties.description)}</strong></div>
+      </div>
+      <div id="Gantt-${this.instanceId}"> </div>
+    </section>`;
+
+    
+    let data: Object[]  = [
+      {
+          TaskID: 1,
+          TaskName: 'Project Initiation',
+          StartDate: new Date('04/02/2019'),
+          EndDate: new Date('04/21/2019'),
+          subtasks: [
+              { TaskID: 2, TaskName: 'Identify Site location', StartDate: new Date('04/02/2019'), Duration: 4, Progress: 50 },
+              { TaskID: 3, TaskName: 'Perform Soil test', StartDate: new Date('04/02/2019'), Duration: 4, Progress: 50  },
+              { TaskID: 4, TaskName: 'Soil test approval', StartDate: new Date('04/02/2019'), Duration: 4 , Progress: 50 },
+          ]
+      },
+      {
+          TaskID: 5,
+          TaskName: 'Project Estimation',
+          StartDate: new Date('04/02/2019'),
+          EndDate: new Date('04/21/2019'),
+          subtasks: [
+              { TaskID: 6, TaskName: 'Develop floor plan for estimation', StartDate: new Date('04/04/2019'), Duration: 3, Progress: 50 },
+              { TaskID: 7, TaskName: 'List materials', StartDate: new Date('04/04/2019'), Duration: 3, Progress: 50 },
+              { TaskID: 8, TaskName: 'Estimation approval', StartDate: new Date('04/04/2019'), Duration: 3, Progress: 50 }
+          ]
+      },
+  ];
+  Gantt.Inject(Sort);
+  let gantt: Gantt = new Gantt({
+       dataSource: data,
+       allowSorting: this.properties.allowSorting,
+       taskFields: {
+          id: 'TaskID',
+          name: 'TaskName',
+          startDate: 'StartDate',
+          duration: 'Duration',
+          dependency: 'Predecessor',
+          progress: 'Progress',
+          child: 'subtasks',
+      },
+   });
+
+gantt.appendTo('#Gantt-'+this.instanceId);
+  }
+
+  protected onInit(): Promise<void> {
+    this._environmentMessage = this._getEnvironmentMessage();
+
+this.properties.allowSorting = true;
+    return super.onInit();
+  }
+
+
+
+  private _getEnvironmentMessage(): string {
+    if (!!this.context.sdks.microsoftTeams) { // running in Teams
+      return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+    }
+
+    return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment;
+  }
+
+  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+    if (!currentTheme) {
+      return;
+    }
+
+    this._isDarkTheme = !!currentTheme.isInverted;
+    const {
+      semanticColors
+    } = currentTheme;
+
+    if (semanticColors) {
+      this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
+      this.domElement.style.setProperty('--link', semanticColors.link || null);
+      this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
+    }
+
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                }),
+                PropertyPaneCheckbox('allowSorting', {
+                 checked: this.properties.allowSorting,
+                 text: "Sorting in Gantt"
+                }),
+
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/ganttChart/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IGanttChartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'GanttChartWebPartStrings' {
+  const strings: IGanttChartWebPartStrings;
+  export = strings;
+}
